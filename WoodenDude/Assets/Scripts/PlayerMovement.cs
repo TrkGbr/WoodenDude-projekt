c02b93a43c7ace0bfb46c6586a35c559ee5fddbe
Máté <mate8588@gmail.com>
2024-04-03 13:59:40 +0200

    Movement/Colliders
    update

--- WoodenDude/Assets/Scripts/PlayerMovement.cs
+++ WoodenDude/Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     //Fizika hozzárendelése a karakterhez
     public Rigidbody2D rb;
-    public Animator animator;
+    //public Animator animator;
 
     Vector2 movement;
     
@@ -18,9 +18,9 @@ public class PlayerMovement : MonoBehaviour
        movement.x = Input.GetAxisRaw("Horizontal");
        movement.y = Input.GetAxisRaw("Vertical");
 
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        //animator.SetFloat("Horizontal", movement.x);
+        //animator.SetFloat("Vertical", movement.y);
+        //animator.SetFloat("Speed", movement.sqrMagnitude);
 
         //normalized a keresztirányú mozgásban segít(enélkül a karakter 40 % -al gyorsabb keresztirányban)
         movement = new Vector2 (movement.x, movement.y).normalized;
